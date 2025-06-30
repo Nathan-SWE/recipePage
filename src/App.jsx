bb76b19e2db276fe0fbbac941ed2204aeac26592
Nathan Lima <nathanls.swe@gmail.com>
2025-06-30 19:38:06 -0300

    feat: create Instructions component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header";
 import Ingredients from "./components/Ingredients/Ingredients";
+import Instructions from "./components/Instructions/Instructions";
 import Preparation from "./components/Preparation/Preparation";
 import Recipe from "./components/Recipe/Recipe";
 
@@ -11,24 +12,7 @@ function App() {
         <Recipe />
         <Preparation />
         <Ingredients />
-
-        <section>
-          <h2>Instructions</h2>
-          <p>
-            Beat the eggs: In a bowl, beat the eggs with a pinch of salt and
-            pepper until they are well mixed. You can add a tablespoon of water
-            or milk for a fluffier texture. Heat the pan: Place a non-stick
-            frying pan over medium heat and add butter or oil. Cook the
-            omelette: Once the butter is melted and bubbling, pour in the eggs.
-            Tilt the pan to ensure the eggs evenly coat the surface. Add
-            fillings (optional): When the eggs begin to set at the edges but are
-            still slightly runny in the middle, sprinkle your chosen fillings
-            over one half of the omelette. Fold and serve: As the omelette
-            continues to cook, carefully lift one edge and fold it over the
-            fillings. Let it cook for another minute, then slide it onto a
-            plate. Enjoy: Serve hot, with additional salt and pepper if needed.
-          </p>
-        </section>
+        <Instructions />
 
         <section>
           <h2>Nutrition</h2>
