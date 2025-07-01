f63b1a0e40be303f1f7753503faaa552b2ed80cb
Nathan Lima <nathanls.swe@gmail.com>
2025-07-01 12:13:41 -0300

    fix: define external link rel attributes for security

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,13 +5,21 @@ export default function Footer() {
     <footer>
       <span>
         Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Frontend Mentor
         </a>
       </span>
       <span>
         Coded by{" "}
-        <a href="https://github.com/nathan-swe" target="_blank">
+        <a
+          href="https://github.com/nathan-swe"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Nathan
         </a>
       </span>
