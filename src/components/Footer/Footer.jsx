ee5bb19240bdf01ab41d134c21162b06c6cc85ba
Nathan Lima <nathanls.swe@gmail.com>
2025-06-30 22:21:00 -0300

    style: add Footer component styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,20 @@
+import "./Footer.css";
+
 export default function Footer() {
   return (
     <footer>
-      Challenge by
-      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-        Frontend Mentor
-      </a>
-      . Coded by{" "}
-      <a href="https://github.com/nathan-swe" target="_blank">
-        Nathan
-      </a>
-      .
+      <span>
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+      </span>
+      <span>
+        Coded by{" "}
+        <a href="https://github.com/nathan-swe" target="_blank">
+          Nathan
+        </a>
+      </span>
     </footer>
   );
 }
